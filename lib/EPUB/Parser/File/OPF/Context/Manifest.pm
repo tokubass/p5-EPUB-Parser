a3dea4815875c4b8af192c657eea5c21695d8546
tokubass <tomi21110@gmail.com>
2013-06-16 23:59:23 +0900

    fixed document

--- lib/EPUB/Parser/File/OPF/Context/Manifest.pm
+++ lib/EPUB/Parser/File/OPF/Context/Manifest.pm
@@ -127,45 +127,64 @@ __END__
 
 =head1 METHODS
 
-=head2 new(\%opts)
-
-Constructor.
-This method called from L<EPUB::Parser::File::OPF> object.
-my $manifest = $epub_parser->opf->manifest;
-
 =head2 nav_path
 
-Get navigation file path from item with the property 'nav'.
+Get navigation file path from item element with the property 'nav'.
 
 =head2 cover_image_path
 
-Get cover image path from item with the property 'cover-image'.
+Get cover image path from item element with the property 'cover-image'.
 
-=head2 attr_by_media_tyep
+=head2 attr_by_media_tyep({ regexp => qr{ ... }ix })
 
-{
+Retrun in the following format.
+ {
 
     'image/png' =>  [{
         href => "cover.png", id => "_cover.png", properties => "cover-image"
-    }, {
+    },{
         href => "fig01.png", id => "_fig01.png"
     }],
     'text/css' => [{
         .....
     }],
-}
+ }
 
 =head2 attr_by_id
 
+Retrun in the following format.
+ {
+    "_cover.png"   => { "media-type" => "image/png", href => "cover.png", properties => "cover-image" },
+    "_style.css"   => { "media-type" => "text/css",  href => "style.css" },
+    "_toc.ncx"     => { "media-type" => "application/x-dtbncx+xml", href => "toc.ncx" },
+    "_cover.xhtml" => { "media-type" => "application/xhtml+xml",  href => "cover.xhtml" },
+    "_nav.xhtml"   => { "media-type" => "application/xhtml+xml",  href => "nav.xhtml", properties => "nav" },
+    ....
+ }
+
+
 =head2 items_path
+
+Returns all item path.
 
 =head2 items_path_by_media_type
 
+Returns items path with media-type specified by Regular expression.
+
 =head2 items
+
+Returns all item.
+The item is instance of L<EPUB::Parser::Util::Archive::Iterator>.
 
 =head2 items_by_media
 
-=head2 items_by_media_type
+Returns items with specified media-type. image/*, video/* ,audio/* .
+The item is instance of L<EPUB::Parser::Util::Archive::Iterator>.
+
+=head2 items_by_media_type({ regexp => qr{ ... }ix })
+
+Returns items with media-type specified by Regular expression.
+The item is instance of L<EPUB::Parser::Util::Archive::Iterator>.
 
 =head1 LICENSE
 
--- lib/EPUB/Parser/File/OPF/Context/Metadata.pm
+++ lib/EPUB/Parser/File/OPF/Context/Metadata.pm
@@ -12,3 +12,44 @@ sub identifier { shift->parser->single( 'dc:identifier' )->string_value }
 
 
 1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+ EPUB::Parser::File::OPF::Context::Metadata - parses metadata node in opf file
+
+=head1 METHODS
+
+=head2 title
+
+return value of dc:title.
+
+=head2 creator
+
+return value of dc:creator.
+
+=head2 language
+
+return value of dc:language.
+
+=head2 identifier
+
+return value of dc:identifier
+
+=head1 LICENSE
+
+Copyright (C) tokubass.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+tokubass E<lt>tokubass {at} cpan.orgE<gt>
+
+=cut
+
+
