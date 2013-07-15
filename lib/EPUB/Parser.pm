572e86d03a6a75129041cc5288305dd6c8f77f64
tokubass <tomi21110@gmail.com>
2013-07-15 16:23:02 +0900

    add shortcut method

--- lib/EPUB/Parser.pm
+++ lib/EPUB/Parser.pm
@@ -10,6 +10,8 @@ use EPUB::Parser::File::Navi;
 use EPUB::Parser::Manager::Pages;
 use EPUB::Parser::Util::ShortcutMethod qw/
     title creator language identifier
+    items_by_media items_by_media_type
+    toc_list
 /;
 
 
@@ -95,18 +97,18 @@ __END__
  my $version = $ep->opf->guess_version;
 
  # get css. Return value is 'EPUB::Parser::Util::Archive::Iterator' object.
- my $itr = $ep->opf->manifest->items_by_media_type({ regexp => qr{text/css}ix });
+ my $itr = $ep->items_by_media_type({ regexp => qr{text/css}ix });
  while ( my $zip_member = $itr->next ) {
      $zip_member->data;
      $zip_member->path;
  }
 
  # shortcut method. iterator object contain image,audio,video item path.
- my $itr = $ep->opf->manifest->items_by_media;
+ my $itr = $ep->items_by_media;
 
  # get list under <nav id="toc" epub:type="toc"> 
  # todo: parse nested list
- for my $chapter ( @{$ep->navi->toc->list} ) {
+ for my $chapter ( $ep->toc_list ) {
      $chapter->{title};
      $chapter->{href};
  }
--- lib/EPUB/Parser/Util/ShortcutMethod.pm
+++ lib/EPUB/Parser/Util/ShortcutMethod.pm
@@ -3,14 +3,31 @@ use strict;
 use warnings;
 use Exporter qw/import/;
 
-our @EXPORT_OK = qw/ title creator language identifier /;
+our @EXPORT_OK = qw/
+ title creator language identifier
+ items_by_media items_by_media_type
+ toc_list
+ /;
 
-
+## metadata
 sub title      { shift->opf->metadata->title      }
 sub creator    { shift->opf->metadata->creator    }
 sub language   { shift->opf->metadata->language   }
 sub identifier { shift->opf->metadata->identifier }
 
+
+## manifest
+sub items_by_media {
+    shift->opf->manifest->items_by_media;
+}
+
+sub items_by_media_type {
+    shift->opf->manifest->items_by_media_type(@_);
+}
+
+
+## navi
+sub toc_list { shift->navi->toc->list }
 
 
 1;
